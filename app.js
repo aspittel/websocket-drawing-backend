b369b4f81b12c9fa3b3e838111fbb87737a9d23e
aspittel <ali07cat07@gmail.com>
2017-11-07 09:09:46 -0500

    adding cors

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
-var app = require('express')()
-var http = require('http').Server(app)
-var io = require('socket.io')(http)
+const app = require('express')()
+const http = require('http').Server(app)
+const io = require('socket.io')(http)
+const cors = require('cors')
 
+app.use(cors())
 
 io.on('connection', (socket) => {
   console.log('a user connected')
